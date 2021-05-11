88e593e5eb8f94bfad5ebd1ebb032d7aa0bf5690
sofiabukovskaya <sofi454545@gmail.com>
2021-05-11 18:32:34 +0300

    Load image from Internet, was added mixin TickerProviderState

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animation/ui/widgets/cat.dart';
 
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+    return HomePageState();
   }
-  
 }
 
-class HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> with TickerProviderStateMixin{
+  Animation<double> catAnimation;
+  AnimationController catAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    catAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation'),
+      ),
+      body: buildAnimation(),
+    );
   }
-  
+
+  Widget buildAnimation() {
+    return Cat();
+  }
 }
